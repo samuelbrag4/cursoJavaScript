795a0122815256dcc22e133d8abe6eaa89aa5c20
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2024-11-18 17:08:00 -0300

    feat: added data type number

---
+++ aulas/introducao/tiposDeDados/number.js
@@ -0,0 +1,24 @@
+// Representa valores numericos
+
+const numero = 10;
+// 10 é um número positivo
+
+const numeroNegativo = -10;
+// -10 é um número negativo
+
+const numeroDecimal = 10.5;
+// 10.5 é um número decimal
+
+const numeroNegativoDecimal = -10.5;
+// -10.5 é um número decimal negativo
+
+const numeroExpoente = 2e3;
+// 2e3 é igual a 2 * 10^3 = 2000
+
+
+// typeof é um operador que retorna o tipo de dado de uma variável
+console.log(typeof(numero));
+console.log(typeof(numeroNegativo));
+console.log(typeof(numeroDecimal));
+console.log(typeof(numeroNegativoDecimal));
+console.log(typeof(numeroExpoente));
