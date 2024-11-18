f14a76ad124920c7b7410b087ba6b52a141e9344
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2024-11-18 17:24:43 -0300

    feat: added data type number complement

---
+++ aulas/introducao/tiposDeDados/numbers/specialNumbers.js
@@ -0,0 +1,11 @@
+// Numeros: especiais
+
+// infinito
+// - infinito
+// NaN (not a number)
+
+// Testes
+
+console.log(typeof(Infinity)); // Infinity
+console.log(typeof(-Infinity)); // -Infinity
+console.log(typeof(NaN)); // NaN
