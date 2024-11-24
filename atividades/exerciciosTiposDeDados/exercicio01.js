72179ba1911ccf4b7fa3c509f77c0ae5874be7d7
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2024-11-24 12:28:48 -0300

    feat: Add exercise of displaying strings with different types of quotes in JavaScript

---
+++ atividades/exerciciosTiposDeDados/exercicio01.js
@@ -0,0 +1,18 @@
+// Exercício01
+
+/*
+Escreva três valores em string em um arquivo e exiba com console.log
+Com aspas duplas, simples e template literals
+*/
+
+const nome = "Samuel"
+const sobrenome = 'Braga'
+const linguagem = `JavaScript`
+
+console.log(nome)
+console.log(sobrenome)
+console.log(linguagem)
+
+// Tudo junto
+
+console.log(`Olá! Meu nome é ${nome} ${sobrenome} e eu programo em ${linguagem}`)
