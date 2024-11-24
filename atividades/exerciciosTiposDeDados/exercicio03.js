01a70b13aabd6b5473f80ebcc9fda6e0ad1c3663
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2024-11-24 12:35:10 -0300

    feat: Add boolean comparisons exercise in JavaScript

---
+++ atividades/exerciciosTiposDeDados/exercicio03.js
@@ -0,0 +1,17 @@
+// Exercício03
+
+/*
+Escreva três comparações com boolean.
+Uma com maior, menor ou igual e diferente.
+*/
+
+let a = 10;
+let b = 20;
+
+console.log(a > b); // false
+console.log(a <= b); // true
+console.log(a != b); // true
+
+// Tudo junto
+
+console.log(a > b, a <= b, a != b); // false true true
