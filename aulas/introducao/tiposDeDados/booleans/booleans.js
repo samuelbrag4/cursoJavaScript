ae2f8f6076ee0958a9eb9a9619150865b3ab4eae
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2024-11-24 11:32:55 -0300

    feat: adds examples of using the boolean data type in JavaScript

---
+++ aulas/introducao/tiposDeDados/booleans/booleans.js
@@ -0,0 +1,28 @@
+// Bolleans são tipos de dados que representam verdadeiro ou falso
+// São muito utilizados em estruturas condicionais
+
+// Exemplo de uso de booleans
+console.log(5 > 3); // true
+console.log(5 < 3); // false
+
+console.log(typeof(5 > 3));
+console.log(typeof(5 < 3));
+
+// Outro exemplo
+console.log(true);
+console.log(false);
+
+console.log(typeof(true));
+console.log(typeof(false));
+
+// Boolean em condicionais
+let falso = false;
+
+if (falso == true) {
+    console.log("É trueeeee")
+} else {
+    console.log("É falseeee")
+}
+
+// Exemplo de uso de booleans em variáveis
+let verdadeiro = true;
---
+++ observacoes/caixaComentário.js
@@ -0,0 +1,6 @@
+/* 
+Essa é uma caixa de comentário
+Posso falar o quanto eu quiser aqui dentro
+*/
+
+let texto = "Agora estou fora da caixa de comentário!";
