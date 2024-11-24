767ef33d057c46e2221880124e5d56a6f9155a9c
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2024-11-24 12:18:57 -0300

    feat: Add examples of automatic type conversion and empty values ​​in JavaScript

---
+++ observacoes/conversaoDeTipoAutomatica.js
@@ -0,0 +1,6 @@
+// Conversão de Tipo Automática
+
+console.log(1 + '1'); // 11
+console.log(5 * null); // 0
+console.log('5' - 1); // 4
+console.log("Dois" * "Tres"); // NaN
---
+++ observacoes/emptyValues.js
@@ -0,0 +1,7 @@
+// Empty Values são valores que não possuem um valor, ou seja, são vazios.
+
+console.log(null); // null
+console.log(undefined); // undefined
+
+console.log(typeof(null)); // null
+console.log(typeof(undefined)); // undefined
