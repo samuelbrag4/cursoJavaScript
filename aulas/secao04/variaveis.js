e4f2fe4e37678f9fea9cf786b0159a8391f39297
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2024-11-27 16:20:50 -0300

    feat: Add example of variables in JavaScript with var, let and const

---
+++ aulas/secao04/variaveis.js
@@ -0,0 +1,25 @@
+// Variáveis em JavaScript
+
+// Elas podem ser declaradas de 3 formas diferentes:
+// var-> variável global
+// let-> variável local
+// const-> variável constante
+// Normalmente, a mais utilizada é a let
+
+// var
+var nome = 'Luiz';
+
+// let
+let idade = 25;
+
+// const
+const dataNascimento = '01/01/1995';
+
+// Exibindo as variáveis
+console.log(nome, idade, dataNascimento);
+
+// Alterando o valor de uma variável
+idade = idade + 1;
+
+// Nova idade
+console.log(idade);
