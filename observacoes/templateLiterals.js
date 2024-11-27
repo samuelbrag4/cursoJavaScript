74c4c1b306b37a1588c401674d3bd557d59381d1
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2024-11-27 16:23:01 -0300

    feat: Add example of JavaScript template literals

---
+++ observacoes/templateLiterals.js
@@ -0,0 +1,9 @@
+// template literals
+// template literals
+const nome = "João";
+const idade = 30;
+const cidade = "São Paulo";
+
+const mensagem = `Meu nome é ${nome}, tenho ${idade} anos e moro em ${cidade}.`;
+
+console.log(mensagem);
