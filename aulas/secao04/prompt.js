4113db8a1125b798cf2ec08aaa0324c89672fd96
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2024-11-27 16:59:37 -0300

    feat: Add example of using the built-in prompt function in JavaScript

---
+++ aulas/secao04/prompt.js
@@ -0,0 +1,9 @@
+// Função built-in: Prompt
+
+// Prompt é uma função built-in que exibe uma caixa de diálogo para o usuário inserir um texto. O texto inserido é retornado pela função.
+
+// Exemplo:
+let nome = prompt('Digite seu nome:');
+
+// Exibição
+console.log(nome);
