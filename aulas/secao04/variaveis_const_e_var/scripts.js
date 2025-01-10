5e2ef085e4477e087aaab42cd26895e238342fbd
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2025-01-10 17:31:25 -0300

    feat: Add example of using const and var variables in JavaScript

---
+++ aulas/secao04/variaveis_const_e_var/scripts.js
@@ -0,0 +1,13 @@
+var nome = 'Samuel';
+
+console.log(nome);
+
+nome = 'Maria';
+
+console.log(nome);
+
+const idade = 60;
+
+console.log(idade);
+
+console.log('Meu nome é ' + nome + ' e tenho ' + idade + ' anos.');
