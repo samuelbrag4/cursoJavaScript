ccc0d46e20aa4cff890f99e2faabb2d45251e208
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2025-01-10 18:09:54 -0300

    feat: Add example of using prompt to check age in JavaScript

---
+++ aulas/secao04/prompt/scripts.js
@@ -0,0 +1,15 @@
+// let idade = prompt('OLá! Me diga a sua idade:')
+
+// console.log(idade);
+
+// Forma de fazer o tratamento dos dados atraves do prompt
+
+let idade = prompt('Qual é a sua idade?');
+
+console.log(idade);
+
+if (idade >= 18) {
+    console.log('Você é maior de idade.');
+} else {
+    console.log('Você é menor de idade.');
+}
