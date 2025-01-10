4c7d548efb47ec4bd3e435daef1ad2628db1eef6
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2025-01-10 18:13:13 -0300

    feat: Add example of using alert and prompt to check age in JavaScript

---
+++ aulas/secao04/alert/scripts.js
@@ -0,0 +1,13 @@
+// alert('Cuidado! Sua senha está incorreta!');
+
+// Exemplo de uso do alert
+
+let idade = prompt('Qual é a sua idade?');
+
+console.log(idade);
+
+if (idade >= 18) {
+    alert('Você é maior de idade.');
+} else {
+    alert('Você é menor de idade.');
+}
