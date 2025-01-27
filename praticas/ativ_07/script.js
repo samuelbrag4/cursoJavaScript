46d73981b84b76e6f55ef51945221c9198449d0d
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2025-01-27 03:27:17 -0300

    feat: Add new JavaScript exercises and remove old files

---
+++ praticas/ativ_07/script.js
@@ -0,0 +1,6 @@
+/*
+Inversor de String
+
+Peça ao usuário uma palavra e exiba ela invertida.
+(Exemplo: "JavaScript" → "tpircSavaJ")
+*/
--- praticas/ativ_10/script.js
+++ praticas/ativ_10/script.js
@@ -1,27 +1,5 @@
 /*
-Jogo de Adivinhação
+Jogo do Par ou Ímpar
 
-Gere um número aleatório entre 1 e 10. O usuário deve tentar adivinhar esse número. A cada tentativa, o programa deve dizer se o número é maior ou menor. O jogo continua até o usuário acertar.
+Crie um jogo onde o usuário escolhe "par" ou "ímpar" e digita um número. O computador gera um número aleatório entre 1 e 10. Some os dois números e diga quem ganhou!
 */
-
-let randomNumber = Math.floor(Math.random() * 10) + 1;
-
-// Divida: O que o .floor() faz? O que o .random() faz? O que o + 1 faz?
-
-let input = parseInt(prompt('Tente adivinhar o numero: '));
-
-let attempts = 1;
-
-while (input !== randomNumber) {
-    if (input < randomNumber) {
-        input = parseInt(prompt('Tente um número maior'));
-    }
-
-    if (input > randomNumber) {
-        input = parseInt(prompt('Tente um número menor'));
-    }
-
-    attempts++;
-}
-
-console.log(`Parabéns! Você acertou em ${attempts} tentativas!`);
